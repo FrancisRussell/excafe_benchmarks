086e94ca9724c3bca85f39cae2e89bb06e2d62cd
Francis Russell <francis@unchartedbackwaters.co.uk>
2012-06-20 19:09:49 +0100

    Make generate_ufl_rules.sh correctly use echo escapes.

--- mass_matrix_2d/generate_ufl_rules.sh
+++ mass_matrix_2d/generate_ufl_rules.sh
@@ -23,7 +23,7 @@ for NF in 1 2 3 4; do
       TEMPLATE_GENERATED="${UFL_FILE} ${BENCHMARK_SOURCE}"
 
       echo "${UFL_FILE}: ${TEMPLATE_FILE}"
-      echo "\tm4 -DP_VALUE=${P} -DQ_VALUE=${Q} \$^ > \$@"
+      echo -e "\tm4 -DP_VALUE=${P} -DQ_VALUE=${Q} \$^ > \$@"
 
       FFC_BUILT_SOURCES=""
       for REPRESENTATION in tensor quadrature; do
@@ -34,26 +34,26 @@ for NF in 1 2 3 4; do
 
         # We need two copies, one for quadrature, one for tensor.
         echo "${REP_SPECIFIC_UFL_FILE}: ${UFL_FILE}"
-        echo "\tcp \$^ \$@"
+        echo -e "\tcp \$^ \$@"
 
         FFC_HEADER="mass_matrix_${REP_SPECIFIC_OPTIONS}.h"
         FFC_BUILT_SOURCES="${FFC_HEADER} ${FFC_BUILT_SOURCES}"
 
         echo "${FFC_HEADER}: ${REP_SPECIFIC_UFL_FILE}"
-        echo "\tffc --optimize -r ${REPRESENTATION} --language ufc \$^" 
+        echo -e "\tffc --optimize -r ${REPRESENTATION} --language ufc \$^" 
       done
 
       # Generate Excafe headers
       EXCAFE_HEADER="mass_matrix_${OPTIONS}_excafe.h"
       EXCAFE_BUILT_SOURCES="${EXCAFE_HEADER}"
       echo "${EXCAFE_HEADER}:"
-      echo "\t\${MASS_MATRIX_2D_GENERATOR} ${NF} ${P} ${Q} ${EXCAFE_HEADER}"
+      echo -e "\t\${MASS_MATRIX_2D_GENERATOR} ${NF} ${P} ${Q} ${EXCAFE_HEADER}"
 
       # Generate dependencies for benchmark executable
       echo "${BENCHMARK_SOURCE}: ${BENCHMARK_TEMPLATE}"
-      echo "\tm4 -DNF_VALUE=${NF} -DP_VALUE=${P} -DQ_VALUE=${Q} \$^ > \$@"
+      echo -e "\tm4 -DNF_VALUE=${NF} -DP_VALUE=${P} -DQ_VALUE=${Q} \$^ > \$@"
       echo "${BENCHMARK_EXECUTABLE}: ${BENCHMARK_SOURCE} ${FFC_BUILT_SOURCES} ${EXCAFE_BUILT_SOURCES}"
-      echo '\t${CXX} ${CXXFLAGS} ${LDFLAGS} $< -o $@'
+      echo -e '\t${CXX} ${CXXFLAGS} ${LDFLAGS} $< -o $@'
 
       CLEAN_FILES="${BENCHMARK_EXECUTABLE} ${TEMPLATE_GENERATED} ${CLEAN_FILES}"
       CLEAN_FILES="${FFC_BUILT_SOURCES} ${EXCAFE_BUILT_SOURCES} ${CLEAN_FILES}"
